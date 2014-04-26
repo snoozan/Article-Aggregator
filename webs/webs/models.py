2b422d2c4a26f68996677bbb8d6ec74e532bbc72
Susan Lunn <slgems@gmail.com>
2014-04-26 13:58:01 -0400

    contains items and models

---
+++ webs/webs/models.py
@@ -0,0 +1,32 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.engine.url import URL
+from sqlalchemy import create_engine, Column, String
+
+import settings
+
+def db_connect():
+    """
+    connects engine to postgres database in settings
+    """
+    return create_engine(URL(**settings.DATABASE))
+
+DeclarativeBase = declarative_base
+
+def create_url_table(engine):
+
+    """
+    creates the table and its metadata
+    """
+    DeclarativeBase.metadata.create_all(engine)
+
+class URLs(DeclarativeBase):
+
+    """
+    table full of article urls
+    """
+
+    __tablename__ = "urls"
+
+    url = Column(String, primary_key=True)
+
+
