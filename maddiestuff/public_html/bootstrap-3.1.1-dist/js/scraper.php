00bbb9fd53e990cea934213e2c22926e13af8dbe
Susan Lunn <slgems@gmail.com>
2014-10-02 15:31:59 -0400

    deleted maddiestuff

--- maddiestuff/public_html/bootstrap-3.1.1-dist/js/scraper.php
+++
@@ -1,7 +0,0 @@
-<?
-	Header("content-type: application/x-javascript");
-	$tempJsonArray = row_to_json(row("http://spectrum.ieee.org/aerospace/satellites/japans-plan-for-centimeterresolution-gps", "Japan’s Plan for Centimeter-Resolution GPS", "A stranger to Tokyo could easily get lost in its urban canyons. And GPS navigation, stymied by low resolution and a blocked view of the sky, might not be much help."));	
-	
-	echo "document.write(\"The Answer is: <b>" . $tempJsonArray . "</b>\")";
-		
-?>
